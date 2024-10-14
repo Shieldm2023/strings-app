6218169f19afcba8e64f5716b542305008286609
Shieldm2023 <77257240+Shieldm2023@users.noreply.github.com>
2024-10-14 16:47:13 -0400

    Update layout and create search-bar

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,6 +1,11 @@
+"use client";
+import { SWRConfig } from "swr";
+
 import Footer from "./footer";
 import Header from "./header";
 import NavBar from "./navbar";
+import fetcher from "../util/fetcher";
+import SearchBar from "./search-bar";
 
 export default function PrivateLayout({
   children,
@@ -8,11 +13,16 @@ export default function PrivateLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <Header />
-      <NavBar />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <SWRConfig value={{ fetcher: fetcher }}>
+      <div className="flex flex-col min-h-screen max-w-md m-auto items-center justify-center">
+        <SearchBar />
+        <Header />
+        <NavBar />
+        <main className="w-full p-5 dark:bg-slate-800 bg-slate-300 rounded-lg my-2">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </SWRConfig>
   );
 }
