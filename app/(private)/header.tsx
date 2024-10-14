cd74418b7c052c93b48d0e580fe7a5667e8fa9b8
Shieldm2023 <77257240+Shieldm2023@users.noreply.github.com>
2024-10-14 16:03:31 -0400

    fetcher

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,18 +1,7 @@
 "use client";
 import useSWR from "swr";
+import fetcher from "../util/fetcher";
 
-const fetcher = async (url: RequestInfo | URL) => {
-  const res = await fetch(url);
-  if (!res.ok) {
-    const msg = "An error occured while fetching data";
-    const info = await res.json();
-    const status = res.status;
-    const error = new Error(msg);
-    console.error(info, status);
-    throw error;
-  }
-  return res.json();
-};
 export default function Header() {
   const { data, error, isLoading } = useSWR("/api/users/profile", fetcher);
 
